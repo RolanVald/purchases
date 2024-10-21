6a3467721169ef30914521a9ee344afd3c382538
RolanVald <roolii95@gmail.com>
2024-10-21 16:24:54 -0600

    Add: spring application

---
+++ src/test/java/com/gastos/tdc/TdcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gastos.tdc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TdcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
